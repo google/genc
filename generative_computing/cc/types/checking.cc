c21aa113663d65f0e38dbb55be279d8301d51752
Krzysztof Ostrowski <ostrowski@google.com>
2023-11-13 18:16:51 +0000

    Adds skeleton C++ classes for typing.
    PiperOrigin-RevId: 582005830

---
+++ generative_computing/cc/types/checking.cc
@@ -0,0 +1,73 @@
+/* Copyright 2023, The Generative Computing Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License
+==============================================================================*/
+
+#include "generative_computing/cc/types/checking.h"
+
+#include "absl/status/status.h"
+#include "absl/strings/str_cat.h"
+#include "generative_computing/proto/v0/computation.pb.h"
+
+namespace generative_computing {
+namespace {
+
+absl::Status CheckScalarEqual(v0::ScalarType x, v0::ScalarType y) {
+  if (x == y) {
+    return absl::OkStatus();
+  }
+  return absl::InvalidArgumentError(
+      absl::StrCat("Scalar ", v0::ScalarType_Name(x),
+                   " vs. a different scalar ", v0::ScalarType_Name(y), "."));
+}
+
+absl::Status CheckStructEqual(v0::StructType x, v0::StructType y) {
+  if (x.element_size() != y.element_size()) {
+    return absl::InvalidArgumentError(
+        absl::StrCat("Inequal number of struct elements ", x.element_size(),
+                     " vs. ", y.element_size(), "."));
+  }
+  for (int i = 0; i < x.element_size(); ++i) {
+    absl::Status status =
+        CheckEqual(x.element(i).value(), y.element(i).value());
+    if (!status.ok()) {
+      return absl::InvalidArgumentError(absl::StrCat(
+          "Type mismatch at struct element ", i, ": ", status.message()));
+    }
+  }
+  return absl::OkStatus();
+}
+
+}  // namespace
+
+absl::Status CheckEqual(v0::Type x, v0::Type y) {
+  if (x.has_scalar()) {
+    if (!y.has_scalar()) {
+      return absl::InvalidArgumentError(
+          absl::StrCat("Scalar ", v0::ScalarType_Name(x.scalar()),
+                       " vs. non-scalar ", y.DebugString(), "."));
+    }
+    return CheckScalarEqual(x.scalar(), y.scalar());
+  }
+  if (x.has_struct_()) {
+    if (!y.has_struct_()) {
+      return absl::InvalidArgumentError(absl::StrCat("Struct ", x.DebugString(),
+                                                     " vs. non-struct ",
+                                                     y.DebugString(), "."));
+    }
+    return CheckStructEqual(x.struct_(), y.struct_());
+  }
+  return absl::UnimplementedError("Not implemented.");
+}
+
+}  // namespace generative_computing
---
+++ generative_computing/cc/types/checking_test.cc
@@ -0,0 +1,98 @@
+/* Copyright 2023, The Generative Computing Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License
+==============================================================================*/
+
+#include "generative_computing/cc/types/checking.h"
+
+#include "googletest/include/gtest/gtest.h"
+#include "absl/status/status.h"
+#include "generative_computing/proto/v0/computation.pb.h"
+
+namespace generative_computing {
+namespace {
+
+class CheckingTest : public ::testing::Test {
+ protected:
+  CheckingTest() {}
+  ~CheckingTest() override {}
+};
+
+TEST_F(CheckingTest, EqualScalars) {
+  v0::Type x, y;
+  x.set_scalar(v0::SCALAR_TYPE_INT32);
+  y.set_scalar(v0::SCALAR_TYPE_INT32);
+  absl::Status status = CheckEqual(x, y);
+  EXPECT_TRUE(status.ok());
+}
+
+TEST_F(CheckingTest, UnequalScalars) {
+  v0::Type x, y;
+  x.set_scalar(v0::SCALAR_TYPE_INT32);
+  y.set_scalar(v0::SCALAR_TYPE_STRING);
+  absl::Status status = CheckEqual(x, y);
+  EXPECT_TRUE(status.code() == absl::StatusCode::kInvalidArgument);
+}
+
+TEST_F(CheckingTest, ScalarAndStruct) {
+  v0::Type x, y;
+  x.set_scalar(v0::SCALAR_TYPE_INT32);
+  y.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  absl::Status status = CheckEqual(x, y);
+  EXPECT_TRUE(status.code() == absl::StatusCode::kInvalidArgument);
+}
+
+TEST_F(CheckingTest, StructAndScalar) {
+  v0::Type x, y;
+  x.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  y.set_scalar(v0::SCALAR_TYPE_INT32);
+  absl::Status status = CheckEqual(x, y);
+  EXPECT_TRUE(status.code() == absl::StatusCode::kInvalidArgument);
+}
+
+TEST_F(CheckingTest, StructsOfUnequalSizes) {
+  v0::Type x, y;
+  x.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  y.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  y.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  absl::Status status = CheckEqual(x, y);
+  EXPECT_TRUE(status.code() == absl::StatusCode::kInvalidArgument);
+}
+
+TEST_F(CheckingTest, StructsWithMismatchingElementTypes) {
+  v0::Type x, y;
+  x.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  y.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_STRING);
+  absl::Status status = CheckEqual(x, y);
+  EXPECT_TRUE(status.code() == absl::StatusCode::kInvalidArgument);
+}
+
+TEST_F(CheckingTest, EqualStructs) {
+  v0::Type x, y;
+  x.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  y.mutable_struct_()->add_element()->mutable_value()->set_scalar(
+      v0::SCALAR_TYPE_INT32);
+  absl::Status status = CheckEqual(x, y);
+  EXPECT_TRUE(status.ok());
+}
+
+}  // namespace
+}  // namespace generative_computing

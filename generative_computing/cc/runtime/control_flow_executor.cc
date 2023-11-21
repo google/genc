8e7e96fb8abc942879b64fa01387e32a44b86506
Ke Wang <kewa@google.com>
2023-11-21 09:47:49 -0800

    enable int_32 in control flow executor.
    PiperOrigin-RevId: 584352213

--- generative_computing/cc/runtime/control_flow_executor.cc
+++ generative_computing/cc/runtime/control_flow_executor.cc
@@ -330,6 +330,7 @@ ControlFlowExecutor::ConstCreateExecutorValue(const v0::Value& value_pb) const {
     case v0::Value::kStr:
     case v0::Value::kBoolean:
     case v0::Value::kTensor:
+    case v0::Value::kInt32:
     case v0::Value::kMedia: {
       return std::make_shared<ExecutorValue>(
           GENC_TRY(child_executor_->CreateValue(value_pb)));

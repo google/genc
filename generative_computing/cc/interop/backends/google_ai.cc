d316148e9d39d7631f5701ae565dce4ac09a1be0
Ke Wang <kewa@google.com>
2024-02-21 09:00:47 -0800

    Consolidate CURL clients
    PiperOrigin-RevId: 609012755

--- generative_computing/cc/interop/backends/google_ai.cc
+++ generative_computing/cc/interop/backends/google_ai.cc
@@ -15,15 +15,14 @@ limitations under the License
 
 #include "generative_computing/cc/interop/backends/google_ai.h"
 
-#include <cstddef>
 #include <string>
 
 #include "absl/status/status.h"
 #include "absl/status/statusor.h"
 #include "absl/strings/substitute.h"
-#include <curl/curl.h>
 #include "generative_computing/cc/intrinsics/model_inference_with_config.h"
 #include "generative_computing/cc/modules/parsers/gemini_parser.h"
+#include "generative_computing/cc/modules/tools/curl_client.h"
 #include "generative_computing/cc/runtime/status_macros.h"
 #include "generative_computing/proto/v0/computation.pb.h"
 
@@ -32,54 +31,6 @@ namespace generative_computing {
 namespace {
 
 constexpr char kGeminiOnAIStudio[] = "/cloud/gemini";
-
-// Callback fn to write the response.
-size_t WriteCallback(void* contents, size_t size, size_t nmemb,
-                     std::string* output) {
-  size_t totalSize = size * nmemb;
-  output->append(static_cast<char*>(contents), totalSize);
-  return totalSize;
-}
-
-absl::StatusOr<v0::Value> Post(const std::string& api_key,
-                               const std::string& endpoint,
-                               const std::string& json_request) {
-  CURL* curl = curl_easy_init();
-
-  if (curl == nullptr) return absl::InternalError("Unable to init CURL");
-
-  curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
-
-  struct curl_slist* headers = nullptr;
-  // API key can be embedded into the URL. Hence empty.
-  if (!api_key.empty()) {
-    headers = curl_slist_append(headers,
-                                ("Authorization: Bearer " + api_key).c_str());
-  }
-  headers = curl_slist_append(headers, "Content-Type: application/json");
-  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_request.c_str());
-
-  // Set the callback function to put the curl response into a buffer.
-  v0::Value response;
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, response.mutable_str());
-
-  // Send the request
-  CURLcode curl_code = curl_easy_perform(curl);
-
-  // Error out if call fails
-  if (curl_code != CURLE_OK) {
-    return absl::InternalError(curl_easy_strerror(curl_code));
-  }
-
-  // Cleanup, free resource and return.
-  curl_slist_free_all(headers);
-  curl_easy_cleanup(curl);
-  return response;
-}
-
 }  // namespace
 
 absl::Status GoogleAI::SetInferenceMap(
@@ -101,7 +52,8 @@ absl::Status GoogleAI::SetInferenceMap(
     const std::string& endpoint = config.struct_().element(0).str() +
                                   "?key=" + config.struct_().element(1).str();
     std::string api_key = "";
-    v0::Value response_json = GENC_TRY(Post(api_key, endpoint, input_json));
+    v0::Value response_json =
+        GENC_TRY(CurlClient::Post(api_key, endpoint, input_json));
 
     // Extract text out of JSON
     return GeminiParser::GetTopCandidateAsText(response_json);
--- generative_computing/cc/intrinsics/rest_call.cc
+++ generative_computing/cc/intrinsics/rest_call.cc
@@ -15,99 +15,15 @@ limitations under the License
 
 #include "generative_computing/cc/intrinsics/rest_call.h"
 
-#include <cstddef>
 #include <string>
 
 #include "absl/status/status.h"
-#include "absl/status/statusor.h"
 #include "absl/strings/str_format.h"
-#include <curl/curl.h>
+#include "generative_computing/cc/modules/tools/curl_client.h"
 #include "generative_computing/cc/runtime/status_macros.h"
 #include "generative_computing/proto/v0/computation.pb.h"
 
 namespace generative_computing {
-
-namespace {
-
-namespace {
-
-// Callback fn to write the response.
-size_t WriteCallback(void* contents, size_t size, size_t nmemb,
-                     std::string* output) {
-  size_t totalSize = size * nmemb;
-  output->append(static_cast<char*>(contents), totalSize);
-  return totalSize;
-}
-}  // namespace
-
-absl::StatusOr<v0::Value> Post(const std::string& api_key,
-                               const std::string& endpoint,
-                               const std::string& json_request) {
-  CURL* curl = curl_easy_init();
-
-  if (curl == nullptr) return absl::InternalError("Unable to init CURL");
-
-  curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
-
-  struct curl_slist* headers = nullptr;
-  // API key can be embedded into the URL. Hence empty.
-  if (!api_key.empty()) {
-    headers = curl_slist_append(headers,
-                                ("Authorization: Bearer " + api_key).c_str());
-  }
-
-  headers = curl_slist_append(headers, "Content-Type: application/json");
-  curl_easy_setopt(curl, CURLOPT_HTTPHEADER, headers);
-
-  curl_easy_setopt(curl, CURLOPT_POSTFIELDS, json_request.c_str());
-
-  // Set the callback function to put the curl response into a buffer.
-  v0::Value response;
-  std::string* response_json = response.mutable_str();
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, response_json);
-
-  // Send the request
-  CURLcode curl_code = curl_easy_perform(curl);
-
-  // Error out if call fails
-  if (curl_code != CURLE_OK) {
-    return absl::InternalError(curl_easy_strerror(curl_code));
-  }
-
-  // Cleanup, free resource and return.
-  curl_slist_free_all(headers);
-  curl_easy_cleanup(curl);
-  return response;
-}
-
-// GET request, API key is embedded in the URL.
-absl::StatusOr<v0::Value> Get(const std::string& endpoint) {
-  CURL* curl = curl_easy_init();
-
-  if (curl == nullptr) return absl::InternalError("Unable to init CURL");
-  curl_easy_setopt(curl, CURLOPT_URL, endpoint.c_str());
-
-  v0::Value response;
-  std::string* response_json = response.mutable_str();
-  curl_easy_setopt(curl, CURLOPT_WRITEFUNCTION, WriteCallback);
-  curl_easy_setopt(curl, CURLOPT_WRITEDATA, response_json);
-  curl_easy_setopt(curl, CURLOPT_HTTPGET, 1L);
-
-  // Send the request
-  CURLcode curl_code = curl_easy_perform(curl);
-
-  // Error out if call fails
-  if (curl_code != CURLE_OK) {
-    return absl::InternalError(curl_easy_strerror(curl_code));
-  }
-
-  // Cleanup, free resource and return
-  curl_easy_cleanup(curl);
-  return response;
-}
-
-}  // namespace
 
 namespace intrinsics {
 absl::Status RestCall::CheckWellFormed(
@@ -128,7 +44,7 @@ absl::Status RestCall::ExecuteCall(const v0::Intrinsic& intrinsic_pb,
   const std::string& url = args.element(1).str();
 
   if (method == kRestCallGet) {
-    *result = GENC_TRY(Get(url));
+    *result = GENC_TRY(CurlClient::Get(url));
     return absl::OkStatus();
   }
 
@@ -137,7 +53,7 @@ absl::Status RestCall::ExecuteCall(const v0::Intrinsic& intrinsic_pb,
         intrinsic_pb.static_parameter().struct_().element(2).str();
     const std::string& json_request_str = arg.str();
 
-    *result = GENC_TRY(Post(api_key, url, json_request_str));
+    *result = GENC_TRY(CurlClient::Post(api_key, url, json_request_str));
     return absl::OkStatus();
   }
 

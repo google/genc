1a8d6cc786a4fa8164e2298f7c0b6c2cf12034eb
Ke Wang <kewa@google.com>
2024-04-16 22:50:42 -0700

    Create threadsafe BiMap. To keep track of authoring context in C++.
    PiperOrigin-RevId: 625566138

---
+++ genc/cc/modules/retrieval/bi_map.h
@@ -0,0 +1,114 @@
+/* Copyright 2023, The GenC Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License
+==============================================================================*/
+
+#ifndef GENC_CC_MODULES_RETRIEVAL_BI_MAP_H_
+#define GENC_CC_MODULES_RETRIEVAL_BI_MAP_H_
+#include <optional>
+
+#include "absl/base/thread_annotations.h"
+#include "absl/container/flat_hash_map.h"
+#include "absl/synchronization/mutex.h"
+
+namespace genc {
+
+// Thread-safe bidirectional map.
+template <typename K, typename V>
+class BiMap {
+ public:
+  explicit BiMap() = default;
+  ~BiMap() = default;
+
+  // Returns true if the insertion is successful.
+  bool Insert(const K& key, const V& value) {
+    absl::MutexLock lock(&mutex_);
+    if (key_to_value_.contains(key) || value_to_key_.contains(value)) {
+      return false;
+    }
+    key_to_value_[key] = value;
+    value_to_key_[value] = key;
+    return true;
+  }
+
+  bool Upsert(const K& key, const V& value) {
+    absl::MutexLock lock(&mutex_);
+
+    auto it_key = key_to_value_.find(key);
+    auto it_value = value_to_key_.find(value);
+
+    if (it_key != key_to_value_.end() && it_value != value_to_key_.end()) {
+      return false;
+    }
+
+    if (it_key != key_to_value_.end() && it_key->second != value) {
+      value_to_key_.erase(it_key->second);
+    }
+
+    if (it_value != value_to_key_.end() && it_value->second != key) {
+      key_to_value_.erase(it_value->second);
+    }
+
+    key_to_value_[key] = value;
+    value_to_key_[value] = key;
+
+    return true;
+  }
+
+  // Removes a pair by Key.
+  bool RemoveByKey(const K& key) {
+    absl::MutexLock lock(&mutex_);
+    auto it = key_to_value_.find(key);
+    if (it == key_to_value_.end()) {
+      return false;
+    }
+
+    value_to_key_.erase(it->second);
+    key_to_value_.erase(it);
+    return true;
+  }
+
+  // Removes a pair by Value.
+  bool RemoveByValue(const V& value) {
+    absl::MutexLock lock(&mutex_);
+    auto it = value_to_key_.find(value);
+    if (it == value_to_key_.end()) {
+      return false;
+    }
+    key_to_value_.erase(it->second);
+    value_to_key_.erase(it);
+    return true;
+  }
+
+  // Finds the Value by Key.
+  std::optional<V> FindByKey(const K& key) const {
+    absl::ReaderMutexLock lock(&mutex_);
+    auto it = key_to_value_.find(key);
+    return it != key_to_value_.end() ? std::optional(it->second) : std::nullopt;
+  }
+
+  // Finds Key by Value.
+  std::optional<K> FindByValue(const V& value) const {
+    absl::ReaderMutexLock lock(&mutex_);
+    auto it = value_to_key_.find(value);
+    return it != value_to_key_.end() ? std::optional(it->second) : std::nullopt;
+  }
+
+ private:
+  mutable absl::Mutex mutex_;
+  absl::flat_hash_map<K, V> key_to_value_ ABSL_GUARDED_BY(mutex_);
+  absl::flat_hash_map<V, K> value_to_key_ ABSL_GUARDED_BY(mutex_);
+};
+}  // namespace genc
+
+#endif  // GENC_CC_MODULES_RETRIEVAL_BI_MAP_H_
---
+++ genc/cc/modules/retrieval/bi_map_test.cc
@@ -0,0 +1,95 @@
+/* Copyright 2023, The GenC Authors.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+     http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License
+==============================================================================*/
+
+#include "genc/cc/modules/retrieval/bi_map.h"
+
+#include <optional>
+#include <string>
+
+#include "googletest/include/gtest/gtest.h"
+
+namespace genc {
+
+namespace {
+
+TEST(BiMapTest, InsertSuccessfully) {
+  BiMap<int, std::string> int_str_map;
+  EXPECT_TRUE(int_str_map.Insert(1, "one"));
+  EXPECT_EQ(int_str_map.FindByKey(1).value(), "one");
+}
+
+TEST(BiMapTest, InsertFailureOnDuplicateKey) {
+  BiMap<int, std::string> int_str_map;
+  int_str_map.Insert(1, "one");
+  EXPECT_FALSE(int_str_map.Insert(1, "duplicate"));
+}
+
+TEST(BiMapTest, InsertFailureOnDuplicateValue) {
+  BiMap<int, std::string> int_str_map;
+  int_str_map.Insert(1, "one");
+  EXPECT_FALSE(int_str_map.Insert(2, "one"));
+}
+
+TEST(BiMapTest, UpsertUpdatesExistingValue) {
+  BiMap<int, std::string> int_str_map;
+  int_str_map.Insert(1, "one");
+  EXPECT_TRUE(int_str_map.Upsert(1, "updated_one"));
+  // EXPECT_EQ(int_str_map.FindByKey(1).value(), "updated_one");
+}
+
+TEST(BiMapTest, UpsertInsertsNewValue) {
+  BiMap<int, std::string> int_str_map;
+  EXPECT_TRUE(int_str_map.Upsert(1, "one"));
+  EXPECT_EQ(int_str_map.FindByKey(1).value(), "one");
+}
+
+TEST(BiMapTest, RemoveByKeyRemovesPair) {
+  BiMap<int, std::string> int_str_map;
+  int_str_map.Insert(1, "one");
+  EXPECT_EQ(int_str_map.FindByKey(1).value(), "one");
+  EXPECT_TRUE(int_str_map.RemoveByKey(1));
+  EXPECT_EQ(int_str_map.FindByKey(1), std::nullopt);
+}
+
+TEST(BiMapTest, RemoveByKeyNotFoundReturnsFalse) {
+  BiMap<int, std::string> int_str_map;
+  EXPECT_FALSE(int_str_map.RemoveByKey(1));
+}
+
+TEST(BiMapTest, RemoveByValueRemovesPair) {
+  BiMap<int, std::string> int_str_map;
+  int_str_map.Insert(1, "one");
+  EXPECT_EQ(int_str_map.FindByValue("one").value(), 1);
+  EXPECT_TRUE(int_str_map.RemoveByValue("one"));
+  EXPECT_EQ(int_str_map.FindByValue("one"), std::nullopt);
+}
+
+TEST(BiMapTest, RemoveByValueNotFoundReturnsFalse) {
+  BiMap<int, std::string> int_str_map;
+  EXPECT_FALSE(int_str_map.RemoveByValue("one"));
+}
+
+TEST(BiMapTest, FindByKeyNotFoundReturnsNullopt) {
+  BiMap<int, std::string> int_str_map;
+  EXPECT_EQ(int_str_map.FindByKey(1), std::nullopt);
+}
+
+TEST(BiMapTest, FindByValueNotFoundReturnsNullopt) {
+  BiMap<int, std::string> int_str_map;
+  EXPECT_EQ(int_str_map.FindByValue("one"), std::nullopt);
+}
+
+}  // namespace
+}  // namespace genc
